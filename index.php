635efa7f8ac23446ec2961f843964a81fce517fb
Brandon Cruz <brcruz@csumb.edu>
2017-02-18 07:38:23 +0000

    Added in tester function to print cards, works properly, can be reused to print individual cards at a later time

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 <?php
 
+$cardArray = array(); //Need an associative array to track suit and card being drawn
+
+function displayCards(){
+    for ($i = 0; $i < 4; $i++)
+    {
+        for ($j = 1; $j <= 13; $j++){
+            echo "<img src='cards/";
+            switch ($i)
+            {
+                case 0:
+                    echo "clubs/";
+                    break;
+                case 1:
+                    echo "diamonds/";
+                    break;
+                case 2:
+                    echo "hearts/";
+                    break;
+                case 3:
+                    echo "spades/";
+                    break;
+            
+            }
+            echo $j. ".png'></link>";
+        }
+    }
+}
+
 
 ?>
 
@@ -7,9 +35,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> </title>
+        <title>SilverJack</title>
+        <link rel="stylesheet" href="lab3.css"></link>
     </head>
     <body>
+        <h1>SilverJack - Nintendo Edition</h1>
+        <?=displayCards()?>
 
     </body>
 </html>
